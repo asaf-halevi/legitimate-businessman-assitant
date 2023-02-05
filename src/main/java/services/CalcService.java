7dd148684daa5aaba5bdbf17f417d885c59a99a8
Halevi, Asaf <asaf.halevi@cellwize.com>
2023-02-05 17:31:10 +0200

    [0001] add code example for TDD

---
+++ src/main/java/services/CalcService.java
@@ -0,0 +1,12 @@
+package services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalcService {
+
+    //TODO - add test before implementation
+    public int absoluteValue(int value) {
+        return 0;
+    }
+}
